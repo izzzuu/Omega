1e997d9f96cac3b265664a091213dde74f3c2ad0
izzzuu <69376797+izzzuu@users.noreply.github.com>
2020-08-08 09:42:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBftYQ2ijTsXtdR7wzuJtNARMKa8bMt0Jk",
+  authDomain: "hack-6942e.firebaseapp.com",
+  databaseURL: "https://hack-6942e.firebaseio.com",
+  projectId: "hack-6942e",
+  storageBucket: "hack-6942e.appspot.com",
+  messagingSenderId: "1028976282003",
+  appId: "1:1028976282003:web:1c1114404a261df1e34066",
+  measurementId: "G-3L8B8H88MP"
 };
 
      
